501afbeeb4160ea3871a0d292454200a654a835a
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-01-21 03:21:28 +0300

    init webpack

---
+++ index.js
@@ -0,0 +1,32 @@
+// import Vue from "vue";
+// import App from "./App.vue";
+// import router from "@/router";
+// import store from "@/store";
+//
+// import '@/assets/scss/index.scss'
+//
+// Vue.filter('formatDate', function (value) {
+//   if (!value) return ''
+//
+//   value = new Date(value)
+//   let options = {
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric"
+//   }
+//   try {
+//     value = value.toLocaleDateString('ru-RU', options)
+//   } catch {
+//     value = 'дата релиза неизвестна'
+//   }
+//
+//
+//
+//   return value
+// })
+//
+// new Vue({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).$mount("#app");
---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+
+<h1>hello friend</h1>
+
+</template>
+
+<script>
+
+export default {
+
+  data:()=> {
+    return {
+
+    };
+  },
+
+
+
+};
+</script>
