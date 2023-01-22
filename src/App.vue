f623a6502c28b004814bdcc268781e51e154556f
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-01-22 19:53:19 +0300

    app bar wip

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-    <v-app :theme="appTheme">
-    <v-app-bar >
-      <h1>abc bar</h1>
+    <v-app :theme="isDarkTheme ? 'dark' : 'light'">
+    <v-app-bar  >
+
+      <v-app-bar-title class="font-weight-black">csy Admin</v-app-bar-title>
+      <template v-slot:append class="d-flex align-center">
+        <v-checkbox hide-details  label="isDarkTheme" v-model="isDarkTheme"/>
+      </template>
+
 
 
     </v-app-bar>
-    <v-navigation-drawer >
-      <v-btn
-          class="ma-2"
-          color="primary"
-      >
-        Accept
-        <v-icon
-            end
-            icon="mdi-checkbox-marked-circle"
-        ></v-icon>
 
-      </v-btn>
-    </v-navigation-drawer>
+      <v-navigation-drawer
+
+      :rail="isSmallNav"
+      >
+          <v-btn
+              class="ma-1 ml-auto d-block"
+
+              :icon="isSmallNav ? 'mdi-chevron-right' : 'mdi-chevron-left'"
+              @click.stop="isSmallNav = !isSmallNav"
+          ></v-btn>
+
+        <v-divider></v-divider>
+        <v-list nav >
+          <v-list-subheader >Каталог</v-list-subheader>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+      </v-navigation-drawer>
+
 
     <v-main>
       <h1>abc</h1>
@@ -33,7 +47,8 @@ export default
 {
 data:()=>{
   return {
-    appTheme:'dark'
+    isDarkTheme:true,
+    isSmallNav:false,
   }
 }
 }
